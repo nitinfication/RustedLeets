c87c160a035e803fc6b7776c80bb4ac9974b2689
Nitin <56484699+nitinfication@users.noreply.github.com>
2024-02-09 23:48:41 +0530

    Time: 7 ms (93.79%), Space: 15.2 MB (74.84%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
@@ -10,31 +10,30 @@
  */
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-     guard let l1 = list1 else { return list2}
-        guard let l2 = list2 else { return list1}
+        guard let l1 = list1 else {return list2}
+        guard let l2 = list2 else {return list1}
         
         let dummyNode = ListNode(0)
         var mergedList: ListNode? = dummyNode
         var l1Curr: ListNode? = l1
         var l2Curr: ListNode? = l2
-        
-        while l1Curr != nil && l2Curr != nil {
-            if l1Curr!.val < l2Curr!.val {
-                mergedList?.next = l1Curr
-                l1Curr = l1Curr?.next
-            } else {
-                mergedList?.next = l2Curr
-                l2Curr = l2Curr?.next
-            }
-            mergedList = mergedList?.next
-        }
-        
-        if l1Curr != nil {
+    while l1Curr != nil && l2Curr != nil {
+        if l1Curr!.val < l2Curr!.val {
             mergedList?.next = l1Curr
-        }
-         if l2Curr != nil {
+            l1Curr = l1Curr?.next
+        } else {
             mergedList?.next = l2Curr
+            l2Curr = l2Curr?.next
         }
+        mergedList = mergedList?.next
+    }
+    if l1Curr != nil {
+        mergedList?.next = l1Curr
+    }
+        
+     if l2Curr != nil {
+        mergedList?.next = l2Curr
+    }
         
         return dummyNode.next
     }
